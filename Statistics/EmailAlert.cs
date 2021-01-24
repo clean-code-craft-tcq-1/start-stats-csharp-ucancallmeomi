6a2a563e9515582d820f0c1e7f4ee4bfa7c57234
Om Prakash <ucancallmeomi@outlook.com>
2021-01-24 18:47:47 +0530

    Added the required code to pass the test cases

---
+++ Statistics/EmailAlert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Statistics
+{
+    public class EmailAlert : IAlerter
+    {
+
+        public bool emailSent = false;
+
+        public void Alert()
+        {
+
+            Console.WriteLine("Email Sent");
+            emailSent = true;
+        }
+
+    }
+}
